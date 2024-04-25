32caba908d40f6713fa5c4474318c1c674cd86fb
Mavgician <jonjon0524@gmail.com>
2024-04-25 18:24:24 +0800

    Added navigationbar settings

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -12,45 +12,58 @@ import {
 } from 'reactstrap';
 
 import { UserAuth } from '@/scripts/AuthContextProvider'
-
-export function Navigationbar() {
+ 
+export function Navigationbar({transparent=false, isFixed=true}) {
   const [isOpen, setIsOpen] = useState(false)
   const auth = UserAuth()
 
-  function toggle() {
-    setIsOpen(!isOpen)
-  }
-  
+  const toggle = () => setIsOpen(!isOpen)
+
   return (
-    <Navbar expand='md' color='dark' dark fixed='top'>
-      <NavbarBrand href='/' className='d-flex align-items-center'>
-        {/* <img src={logo.src} width="40" height="40" /> */}
-        <small className='mx-2'><b>ConFlix</b></small>
-      </NavbarBrand>
-      <NavbarToggler onClick={toggle} />
-      <Collapse isOpen={isOpen} navbar>
-        <Nav className='text-uppercase fw-bold' navbar>
-          <NavItem>
-            <NavLink href='/shopping/women'>Home</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href='/shopping/men'>Tickets</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href='/shopping/kids'>Events</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href='/shopping/kids'>About Us</NavLink>
-          </NavItem>
-        </Nav>
-        <Nav className='ms-auto' navbar>
-          <NavItem>
-            <NavLink href={auth.user ? `/user/${auth.user?.uid}` : '/login'}>
-              muahaha
-            </NavLink>
-          </NavItem>
-        </Nav>
-      </Collapse>
-    </Navbar>
+    <>
+      <Navbar 
+        expand='md'
+        color={transparent ? 'transparent' : 'dark'}
+        dark={transparent ? false : true}
+        fixed={isFixed ? 'top' : undefined}
+        style={transparent ? {boxShadow: 'none'} : null}
+      >
+        <NavbarBrand href='/' className='d-flex align-items-center'>
+          {/* <img src={logo.src} width="40" height="40" /> */}
+          <small className='mx-2'><b>ConFlix</b></small>
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className='text-uppercase fw-bold' navbar>
+            <NavItem>
+              <NavLink href='/shopping/women'>Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href='/shopping/men'>Tickets</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href='/shopping/kids'>Events</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href='/shopping/kids'>About Us</NavLink>
+            </NavItem>
+          </Nav>
+          <Nav className='ms-auto' navbar>
+            <NavItem>
+              <NavLink href={auth.user ? `/user/${auth.user?.uid}` : '/login'}>
+                muahaha
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+      {
+        isFixed ? 
+        <div className='my-3'>
+          &nbsp;
+        </div> :
+        null
+      }
+    </>
   )
 }
