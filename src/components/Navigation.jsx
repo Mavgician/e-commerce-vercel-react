b2daf5462d008bed3223ed468b63b4e6081ba736
Mavgician <jonjon0524@gmail.com>
2024-05-02 14:34:04 +0800

    Fixed navigation links

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import { useState } from 'react';
 import {
@@ -11,41 +11,43 @@ import {
   NavLink,
 } from 'reactstrap';
 
-import { UserAuth } from '@/scripts/AuthContextProvider'
- 
-export function Navigationbar({transparent=false, isFixed=true}) {
-  const [isOpen, setIsOpen] = useState(false)
-  const auth = UserAuth()
+import { UserAuth } from '@/scripts/AuthContextProvider';
 
-  const toggle = () => setIsOpen(!isOpen)
+export function Navigationbar({ transparent = false, isFixed = true }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const auth = UserAuth();
+
+  const toggle = () => setIsOpen(!isOpen);
 
   return (
     <>
-      <Navbar 
+      <Navbar
         expand='md'
         color={transparent ? 'transparent' : 'dark'}
         dark={transparent ? false : true}
         fixed={isFixed ? 'top' : undefined}
-        style={transparent ? {boxShadow: 'none'} : null}
+        style={transparent ? { boxShadow: 'none' } : null}
       >
         <NavbarBrand href='/' className='d-flex align-items-center'>
-          {/* <img src={logo.src} width="40" height="40" /> */}
-          <small className='mx-2'><b>ConFlix</b></small>
+          {/* <img src={logo.src} width='40' height='40' /> */}
+          <small className='mx-2'>
+            <b>ConFlix</b>
+          </small>
         </NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className='text-uppercase fw-bold' navbar>
             <NavItem>
-              <NavLink href='/shopping/women'>Home</NavLink>
+              <NavLink href='/'>Home</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href='/shopping/men'>Tickets</NavLink>
+              <NavLink href='/tickets'>Tickets</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href='/shopping/kids'>Events</NavLink>
+              <NavLink href='/events'>Events</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href='/shopping/kids'>About Us</NavLink>
+              <NavLink href='/about-us'>About Us</NavLink>
             </NavItem>
           </Nav>
           <Nav className='ms-auto' navbar>
@@ -57,13 +59,7 @@ export function Navigationbar({transparent=false, isFixed=true}) {
           </Nav>
         </Collapse>
       </Navbar>
-      {
-        isFixed ? 
-        <div className='my-3'>
-          &nbsp;
-        </div> :
-        null
-      }
+      {isFixed ? <div className='my-3'>&nbsp;</div> : null}
     </>
-  )
+  );
 }
