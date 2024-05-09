ac70c39958bb8a55d1740c996dd310e1a2a3f20a
Mavgician <jonjon0524@gmail.com>
2024-05-09 11:39:20 +0800

    Added page Loading states

--- src/app/(simple)/layout.jsx
+++ src/app/(simple)/layout.jsx
@@ -1,4 +1,13 @@
+'use client'
+
+import { Loader } from '@/components/Loader';
+import { UserAuth } from '@/scripts/AuthContextProvider';
+
 export default function Layout({ children }) {
+  const user = UserAuth()
+
+  if (user.loading) return <Loader/>
+
   return (
     <>
       {children}
--- src/app/(standard)/layout.jsx
+++ src/app/(standard)/layout.jsx
@@ -1,7 +1,15 @@
+'use client'
+
 import { Navigationbar } from '@/components/Navigation';
 import { Footer } from '@/components/Footer';
+import { UserAuth } from '@/scripts/AuthContextProvider';
+import { Loader } from '@/components/Loader';
 
 export default function Layout({ children }) {
+  const user = UserAuth()
+
+  if (user.loading) return <Loader/>
+
   return (
     <>
       <Navigationbar />
