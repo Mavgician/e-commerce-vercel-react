2ce33798e9bb7260c0ea0fd425282cdc353ce9e0
Mavgician <jonjon0524@gmail.com>
2024-05-09 11:41:28 +0800

    Added user page

--- src/app/(standard)/user/[userid]/page.jsx
+++ src/app/(standard)/user/[userid]/page.jsx
@@ -1,28 +1,41 @@
 'use client'
 
-function Page() {
+import { auth } from '@/scripts/firebase';
+
+import { useSignOut } from 'react-firebase-hooks/auth';
+
+import { useRouter } from 'next/navigation'
+
+import {
+  Button,
+  Container
+} from 'reactstrap'
+import { UserAuth } from '@/scripts/AuthContextProvider';
+
+function Page({ params }) {
+  const user = UserAuth()
+
+  const [signOut, loading, error] = useSignOut(auth);
+  const router = useRouter()
+
+  async function logout() {
+    await signOut()
+    router.push('/')
+  }
+
   return (
     <main>
-      {
-      /*
-        Lagay niyo dito pinaka layout ng home page for the store. Naka integrate ung bootstrap dito so all goods tayo dun.
-        Ang hindi lang nakaintegrate sa root component is ung javascript ng bootstrap. So kung mag ca1ousel or something,
-        sabihan niyo ko, kasi medj mahirap iexplain kung paano i allow un - 
-
-        Small explanation:
-          Next.js is mostly server-side rendered, ang pag import ng javascript is not "server" friendly. Need nating i-convert
-          yung component into a client component para maimport ung javascript. This is actually better para forced tayo gumawa
-          ng components for various things on a page. Slideshow component na lalagyan lang ng picture array etc. etc.
-        
-        Watch niyo rin ung sinend ko sa GC natin para mafamiliarize kayo sa framework. I suggest video number 3 para magets niyo
-        ung routing ng pages. Kung paano siya nahahandle ng server ni next.js.
-
-        PAMPADALI TO NG BUHAY NATIN OKAY >:(
-
-        -Mavs ang leader ng bayan
-      */
-      }
-      <h1>You have reached the template page!</h1>
+      <Container className='p-5'>
+        <h1>Hello {user.user?.displayName}</h1>
+        <div className='my-3'>
+          <h3>Actions:</h3>
+          <Button color='danger' size='lg' onClick={logout}>Logout</Button>
+        </div>
+        <div className='my-3'>
+          <h3>Danger Zone:</h3>
+          <Button color='danger' size='lg'>Delete account</Button>
+        </div>
+      </Container>
     </main>
   )
 }
