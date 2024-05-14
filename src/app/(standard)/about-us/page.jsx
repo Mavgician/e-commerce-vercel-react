c7870e27278ac7c077d24d44e1d2ad7ecd3bc8f1
Mavgician <jonjon0524@gmail.com>
2024-05-15 05:00:45 +0800

    Changed some tags

--- src/app/(standard)/about-us/page.jsx
+++ src/app/(standard)/about-us/page.jsx
@@ -1,34 +1,31 @@
+'use client'
+
 import {
   Container
 } from 'reactstrap'
 
-export const metadata = {
-  title: 'ConFlix - About us',
-  description: 'Ticket reseller for concerts',
-};
-
 function Page() {
   return (
-    <main className='d-flex justify-content-center align-items-center'>
+    <main className='d-flex justify-content-center align-itis-center'>
       <Container>
         <div>
           <h3>ABOUT US</h3>
           <p className="text-justify">
-            <strong>ConFlix</strong> was established on the tenets of simplicity, modernity, and an unwavering dedication to quality. Our primary goal is to provide the community with efficient, enticing, and superior concert ticket selling services. We firmly believe in the harmonious blend of quality, efficiency, and simplicity, woven into the very foundation of our company.
+            <b>ConFlix</b> was established on the tenets of simplicity, modernity, and an unwavering dedication to quality. Our primary goal is to provide the community with efficient, enticing, and superior concert ticket selling services. We firmly believe in the harmonious blend of quality, efficiency, and simplicity, woven into the very foundation of our company.
           </p>
           <p>
-            With a strong emphasis on customer satisfaction, user-friendly platforms, and secure transactions, we offer a comprehensive range of concert ticket selling services. At ConFlix, our ethos revolves around a steadfast dedication to modernity. We invest in the latest technology and ensure our platforms stay updated with industry advancements. This dedication ensures effective, precise, and state-of-the-art ticket selling services, guaranteeing optimal experiences for our customers. Quality is the cornerstone of ConFlix; we understand that every transaction, no matter how simple, deserves the utmost care.
+            With a b iphasis on customer satisfaction, user-friendly platforms, and secure transactions, we offer a comprehensive range of concert ticket selling services. At ConFlix, our ethos revolves around a steadfast dedication to modernity. We invest in the latest technology and ensure our platforms stay updated with industry advancients. This dedication ensures effective, precise, and state-of-the-art ticket selling services, guaranteeing optimal experiences for our customers. Quality is the cornerstone of ConFlix; we understand that every transaction, no matter how simple, deserves the utmost care.
           </p>
           <p>
             Our team of knowledgeable and experienced professionals upholds the highest standards of service at ConFlix. We take immense pride in our meticulous processes, fostering trust and satisfaction in a secure and efficient environment.
           </p>
           <p>
-            In addition to our commitment to excellence in ticket selling, we value community engagement and regularly participate in events aimed at promoting music and entertainment. We believe in contributing to the cultural vibrancy of our community and supporting initiatives that enhance the overall experience of concert-goers. ConFlix is not just a ticket selling platform; it's a part of the rich history of music and entertainment.
+            In addition to our commitment to excellence in ticket selling, we value community engagient and regularly participate in events aimed at promoting music and entertainment. We believe in contributing to the cultural vibrancy of our community and supporting initiatives that enhance the overall experience of concert-goers. ConFlix is not just a ticket selling platform; it's a part of the rich history of music and entertainment.
           </p>
           <h3>OUR MISSION</h3>
           <p>
-            "<em>At <strong>ConFlix</strong>,</em> our mission is to <strong>enhance the event experience and overall enjoyment</strong>
-            of our community by providing <em>exceptional ticketing services</em> in a user-friendly and customer-focused
+            "<i>At <b>ConFlix</b>,</i> our mission is to <b>enhance the event experience and overall enjoyment</b>
+            of our community by providing <i>exceptional ticketing services</i> in a user-friendly and customer-focused
             environment. We are dedicated to offering a wide range of event tickets for various interests and
             preferences, ensuring that everyone can find the perfect experience.
           </p>
@@ -39,9 +36,9 @@ function Page() {
             customers.
           </p>
           <p>
-            Our commitment extends beyond ticket sales; we aim to <strong>engage with our customers</strong> and
-            <strong>enhance their overall event experience</strong> through personalized recommendations, event
-            guides, and customer support. By combining technology with empathy, we strive to create memorable
+            Our commitment extends beyond ticket sales; we aim to <b>engage with our customers</b> and
+            <b>enhance their overall event experience</b> through personalized recommendations, event
+            guides, and customer support. By combining technology with ipathy, we strive to create miorable
             experiences and foster a sense of community among event-goers."
           </p>
         </div>
